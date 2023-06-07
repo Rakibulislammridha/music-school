c401e0ab57b0c22a4f77acb4736fbd7046cf3f3d
Rakibulislammridha <dr.rakibulislammridha@gmail.com>
2023-06-07 13:06:18 +0600

    navbar-footer-and-homepage-banner basic implementation

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Shared/footer/Footer';
+import Navbar from '../pages/Shared/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
