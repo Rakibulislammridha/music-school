8b67005889dc29b48642300a03db8487a3a87e7f
Rakibulislammridha <dr.rakibulislammridha@gmail.com>
2023-06-09 02:20:46 +0600

    add class form functionality complte

--- src/components/Form/AddClassForm.jsx
+++ src/components/Form/AddClassForm.jsx
@@ -1,22 +1,21 @@
 import React, { useContext } from "react";
 import { ImSpinner10 } from "react-icons/im";
 import { subjects } from "../Categories/categoriesData";
-import { AuthContext } from "../../providers/AuthProviders";
 
 const AddClassForm = ({
   handleSubmit,
+  user,
   dates,
   handleDates,
   loading = false,
   handleImageChange,
-  uploadButtonText,
+  uploadImageName,
 }) => {
 
-    const {user} = useContext(AuthContext);
 
   return (
     <div className="w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
           <div className="space-y-1 text-sm">
             <label htmlFor="subject" className="block text-gray-600">
@@ -39,6 +38,7 @@ const AddClassForm = ({
               <div className="flex flex-col w-max mx-auto text-center">
                 <label>
                   <input
+                    onChange={(event)=>handleImageChange(event.target.files[0])}
                     className="text-sm cursor-pointer w-36 hidden"
                     type="file"
                     name="image"
@@ -47,7 +47,7 @@ const AddClassForm = ({
                     hidden
                   />
                   <div className="bg-orange-500 text-white border border-orange-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-orange-500">
-                    Upload Image
+                    {uploadImageName}
                   </div>
                 </label>
               </div>
--- src/pages/Dashboard/AddClass.jsx
+++ src/pages/Dashboard/AddClass.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import AddClassForm from '../../components/Form/AddClassForm';
+import { AuthContext } from '../../providers/AuthProviders';
 
 const AddClass = () => {
+
+    const {user, loading, setLoading} = useContext(AuthContext)
+
+    const [uploadImageName, setUploadImageName] = useState("Upload Image")
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        setLoading(true);
+        const form = event.target;
+        const subject = form.subject.value;
+        const instructor = form.instructor.value;
+        const email = form.email.value;
+        const price = form.price.value;
+        const availableSits = form.availableSits.value;
+        const description = form.description.value;
+
+        const image = event.target.image.files[0];
+
+        console.log(subject, instructor, email, price, availableSits, description, image);
+    }
+
+    const handleImageChange = (image) =>{
+        setUploadImageName(image.name)
+    }
+
     return (
         <div>
-            <AddClassForm></AddClassForm>
+            <AddClassForm
+              user={user}
+              loading={loading}
+              handleSubmit={handleSubmit}
+              uploadImageName={uploadImageName}
+              handleImageChange={handleImageChange}
+            ></AddClassForm>
         </div>
     );
 };
