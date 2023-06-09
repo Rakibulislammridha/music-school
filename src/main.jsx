0d942c4e5c10db4d01a16b2b8ac08ee7bbdf26f0
Rakibulislammridha <dr.rakibulislammridha@gmail.com>
2023-06-10 02:03:00 +0600

    promote user to instructor ans promote instructor to admin

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,22 @@ import {
 import { router } from './routes/Router';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProviders from './providers/AuthProviders';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProviders>
     <HelmetProvider>
+    <QueryClientProvider client={queryClient}>
     <div className='max-w-screen-xl mx-auto'>
     <RouterProvider router={router} />
     </div>
+    </QueryClientProvider>
     </HelmetProvider>
     </AuthProviders>
   </React.StrictMode>,
--- src/pages/Dashboard/ManageUsers.jsx
+++ src/pages/Dashboard/ManageUsers.jsx
@@ -1,11 +1,139 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Title from "../../components/Title/Title";
+import { Helmet } from "react-helmet-async";
+import { FaUser, FaUserGraduate, FaUserTie } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const ManageUsers = () => {
-    return (
-        <div>
-            all users
-        </div>
-    );
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      });
+  }, []);
+
+  //   Make admin from instructor
+  const handleMakeAdmin = (user) => {
+    fetch(`${import.meta.env.VITE_API_URL}/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: `${user.name} Now You Are Admin !`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
+  //   Make Instructor from user
+  const handleMakeInstructor = (user) => {
+    fetch(`${import.meta.env.VITE_API_URL}/users/instructor/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: `${user.name} Now You Are Instructor !`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
+  return (
+    <div>
+      <Helmet>
+        <title>Music School | Manage Users</title>
+      </Helmet>
+      <Title subHeading={"There Is Our"} heading={"All Users"}></Title>
+      <div className="">
+        <h3 className="text-4xl text-center">Total Users: {users.length}</h3>
+      </div>
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User Info</th>
+              <th>email</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, i) => (
+              <tr key={user._id}>
+                <th>{i + 1}</th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={user.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{user.name}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{user.email}</td>
+                <td>
+                  {user.role === "admin" && (
+                    <>
+                      <span className="btn btn-sm bg-orange-600 hover:bg-orange-400 text-white">
+                        <FaUserTie></FaUserTie>
+                        Admin
+                      </span>
+                    </>
+                  )}
+                  {user.role === "instructor" && (
+                    <>
+                      <button
+                        onClick={() => handleMakeAdmin(user)}
+                        className="btn btn-sm bg-orange-600 hover:bg-orange-400 text-white"
+                      >
+                        <FaUserGraduate></FaUserGraduate>
+                        Instructor
+                      </button>
+                    </>
+                  )}
+                  <>
+                    <button
+                      onClick={() => handleMakeInstructor(user)}
+                      className="btn btn-sm bg-orange-600 hover:bg-orange-400 text-white"
+                    >
+                      <FaUser></FaUser>
+                      User
+                    </button>
+                  </>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageUsers;
